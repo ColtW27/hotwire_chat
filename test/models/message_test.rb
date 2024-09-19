afb247023ce19c4d0a8884c07a90973f82ee4101
Coltw27 <cwilmouth27@yahoo.com>
2024-09-19 14:54:35 -0400

    add annotations with gem for models and routes

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :integer          not null
+#
+# Indexes
+#
+#  index_messages_on_room_id  (room_id)
+#
+# Foreign Keys
+#
+#  room_id  (room_id => rooms.id)
+#
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
