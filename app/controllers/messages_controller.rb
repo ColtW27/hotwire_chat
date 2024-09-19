0963fa31b0297ab85ce5ff96a0542b5a4b085c58
Coltw27 <cwilmouth27@yahoo.com>
2024-09-19 14:26:59 -0400

    Makes changes to new messages, fix instance varibales

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,14 @@ class MessagesController < ApplicationController
   before_action :set_room, only: %i[ new create ]
 
   def new
-    @message = room.messages.create!(message_params)
+    @message = @room.messages.create!(message_params)
 
     respond_to do |format|
       format.html { redirect_to @room }
     end 
   end
 
-  provate
+  private
     def set_room
       @room = Room.find(params[:room_id])
     end
